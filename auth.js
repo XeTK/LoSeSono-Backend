8de0602c26b80bc544ea42be6683f62f258f9661
Tom Rosier <thr2@aber.ac.uk>
2015-04-16 12:19:16 +0100

    finished commenting auth

--- auth.js
+++ auth.js
@@ -1,16 +1,25 @@
+/*
+ * This deals with the authentication of the user to use the application.
+ * Along with registering the security modules that are needed for the application to work.
+ */
 
 var db = null;
 
+// This sets up the module ready to use.
 function setup(deps) {
 
+	// Get the server module ready to use.
 	var server = deps.server;
 	
+	// Get the database module ready to interface.
 	db = deps.database;
 
+	// This registers the module needed for basic authentication.
 	server.register(
 		require('hapi-auth-basic'), 
 		function (err) {
 
+			// This registers the function needed to authenticate.
 			server.auth.strategy(
 				'simple', 
 				'basic', 
@@ -21,10 +30,12 @@ function setup(deps) {
 		}
 	);
 
+	// This registers the module needed todo cookie sessions with the application.
 	server.register(
 		require('hapi-auth-cookie'), 
 		function (err) {
 
+			// This is the details of the cookie that will be stored.
 			server.auth.strategy(
 				'session', 
 				'cookie', 
@@ -38,6 +49,7 @@ function setup(deps) {
 		}
 	);
 
+	// This is the route that deals with logins for users.
 	server.route(
 		{
 			method: [
@@ -55,7 +67,9 @@ function setup(deps) {
 			},
 			handler: function (request, reply) {
 
+				// Check if the user is authenticated.
 				if (request.auth.isAuthenticated) {
+					// Get the credentials and save it.
 					var account = request.auth.credentials;
 					request.auth.session.set(account);
 					reply(account);
@@ -67,6 +81,7 @@ function setup(deps) {
 		}
 	);
 
+	// This clears the session if the user logs out.
 	server.route(
 		{
 			method: 'GET',
@@ -75,6 +90,7 @@ function setup(deps) {
 				auth: 'session'
 			},
 			handler: function (request, reply) {
+				// Clear the session.
 				request.auth.session.clear();
 
 				return reply.redirect('/');
@@ -82,6 +98,9 @@ function setup(deps) {
 		}
 	);
 
+	// This forces security on the various routes.
+
+	// This overwrites the get routes function.
 	server.getRoute = function(path, callback) {
 
 		server.route(
@@ -98,6 +117,7 @@ function setup(deps) {
 		);
 	};
 
+	// This overwrites the post routes function.
 	server.postRoute = function(path, callback) {
 		server.route(
 			{
@@ -115,8 +135,10 @@ function setup(deps) {
 
 }
 
+// This function validates the user and checks if they are in the database.
 var validate = function (username, password, callback) {
 
+	// Pass the details to the database.
 	db.query(
 		"select valid_password(:username, :password)", 
 		{ 
@@ -129,12 +151,16 @@ var validate = function (username, password, callback) {
 	)
 	.success(
 		function(response) {
+
+			// Get the value of the request to login.
 			var isValid = response[0].valid_password;
 
 			console.log(isValid);
 
+			// If it was a valid login.
 			if (isValid) {
 
+				// Get the users details for the session from the database.
 				db.query(
 					"select u.user_id, \n\
 							u.first_name, \n\
@@ -155,6 +181,8 @@ var validate = function (username, password, callback) {
 				)
 				.success(
 					function(response) {
+
+						// Pass the valid user details back to the method that called authentication which will then store it in session.
 
 						var user = response[0];
 
