e485c930790bc8d961619ed3662df026b69c34e6
Tom Rosier <thr2@aber.ac.uk>
2015-04-19 21:06:09 +0100

    finished commenting comments and votes

--- routes/comment.js
+++ routes/comment.js
@@ -1,44 +1,57 @@
 
 var db = require('../db/comment');
 
+// This is the method that gets called when the module has been loaded.
 function setup(deps) {
 
+	// Get the server from the dependencies so we can use it.
 	var server = deps.server;
 
+	// Pass the list of dependencies to the database functions to setup the database connection.
 	db.setup(deps);
 
+	// Setup the route for getting comments by the message id. It should return a list of comments that are tied to the message.
 	server.getRoute(
 		'/comments/{message_id}',
 		function (request, reply) {
 
+			// Extract the message ID from the parameters that have been given.
 			var msgID = request.params.message_id;
 
+			// Call the database to get all the comments from the database.
 		    db.getComments(
 		    	msgID,
 		    	function(response) {
 		    		//console.log(JSON.Stringify(response));
+		    		// Return the list of comments that we just got from the database.
 		    		reply(response);
 		    	}
 		    );
 		}
 	);
 
+	// This is for adding new comments to a message.
 	server.postRoute(
 		'/comments/add',
 		function (request, reply) {
 
+			// Get the payload with all the details that we want in.
 			var payload = request.payload;
 
+			// Get the user_id from the session as we only want to add messages as the user that is currently logged in.
 			var userID  = request.auth.credentials.user_id;
 
+			// Get the message_id of the message we want to pin the comment to along with the comment we want to add to the message.
 			var msgID   = payload.message_id;
 			var comment = payload.comment;
 
+			// This calls the db and passes the information we need to add the comment to the message.
 			db.addComment(
 				msgID,
 				userID,
 				comment,
 				function(response) {
+					// Return that the job returned successfully.
 					reply({'status':'success'});
 				}
 			);
@@ -46,6 +59,7 @@ function setup(deps) {
 	);
 }
 
+// Make the method accessible to the module loader, for dynamic route loading.
 module.exports = function(route_holder) {
     route_holder['comments'] = setup;
 };
--- routes/vote.js
+++ routes/vote.js
@@ -1,26 +1,46 @@
 
 var db = require('../db/vote');
 
+/* This transposes the information that is taken in from the database.
+ * 
+ *	This:
+ *  [{"type": "positive", "count": "1"}, {"type": "negative", "count": "1"}]
+ *
+ *  To:
+ *  {"positive": 1, "negative": 1}
+ *
+ * This makes it easier to work with inside the android application.
+ */
 function trans(response) {
 
+	// Setup a empty object we can use to add things to.
 	var trans = {}
 
+	// If the response is empty then we don't do any conversion.
 	if (response.length > 0) {
+		// Get the first JSON object in the array to work with.
 		var obj1 = response[0];
 
+		// Convert the count from a string to a number.
 		var count = Number(obj1.count)
 
+		// If the type is positive then we need to get the right keys.
 		if (obj1.vote_type == 'positive') {
+			// So we add a key for positive.
 			trans['positive'] = count;
 		} else {
 			trans['negative'] = count;
 		}
 
+		// If there is two JSON objects then we process the second one.
 		if (response.length > 1) {
+			// Get the JSON object from the array to make it easier to work on.
 			var obj2 = response[1];
 
+			// Do the same again and convert the count to a number so that its easier to work with.
 			count = Number(obj2.count)
 
+			// We check the types so we get the correct keys. For the objects.
 			if (obj2.vote_type == 'positive') {
 				trans['positive'] = count;
 			} else {
@@ -29,40 +49,52 @@ function trans(response) {
 		}
 	}
 
+	// If the object is missing a property that we need then we add it on.
 	if (!trans.hasOwnProperty('positive')) {
 		trans['positive'] = 0;
 	}
 
+	// Same again for the other one.
 	if (!trans.hasOwnProperty('negative')) {
 		trans['negative'] = 0;
 	}
 
+	// Return the transposed object.
 	return trans;
 }
 
+// The method that is run when the module is loaded.
 function setup(deps) {
 
+	// Get the server module so we can add more routes.
 	var server = deps.server;
 
+	// Pass the dependencies to the database so we can setup the database ready to use.
 	db.setup(deps);
 
+	// This is the route for getting votes for a either a message or a comment.
 	server.getRoute(
 		'/vote/{type}/{id}',
 		function (request, reply) {
 
+			// Get the parameters we need to proceed.
 			var voteType = request.params.type;
 			var id       = Number(request.params.id);
 
+			// Workout if we are dealing with a message or a comment, so we can work with the right db methods.
 		    if (voteType == 'comment') {
 
+		    	// Call the database to get the votes related to comments.
 		    	db.getCommentVotes(
 					id,
 					function(response) {
+						// Return the transposed data.
 						reply(trans(response));
 					}
 				);
 
 			} else if (voteType == 'message') {
+				// Do the same for messages.
 				db.getMessageVotes(
 					id,
 					function(response) {
@@ -73,32 +105,42 @@ function setup(deps) {
 		}
 	);
 
+	// This is adding a vote to either a message or comment.
 	server.postRoute(
 		'/vote/{type}/add',
 		function (request, reply) {
 
+			// Get the user id for the currently logged in from the session so we can use it in adding the vote.
 			var userID = Number(request.auth.credentials.user_id);
 		    
+		   	// Get the vote type so either comment or message.
 			var voteType = request.params.type;
 
+			// Get the post parameter for the important information.
 			var payload = request.payload;
 
+			// Get the type of feedback either positive or negative.
 			var type = payload.type;
 			var id   = Number(payload.id);
 
+			// This is the type of vote we are dealing with either a message or a comment vote.
 			if (voteType == 'comment') {
 
+				// Add a vote for comments.
 				db.addCommentVote(
 					id,
 					userID,
 					type,
 					function(response) {
+						// Return that we have proceed successfully.
 						reply({"status":"successful"});
 					}
 				);
 
+			// This is if we are dealing with messages.
 			} else if (voteType == 'message') {
 
+				// Add a vote for messages.
 				db.addMessageVote(
 					id,
 					userID,
@@ -112,6 +154,7 @@ function setup(deps) {
 	);
 }
 
+// This makes the class accessible to the dynamic route loader. So it can be loaded on load of the application.
 module.exports = function(route_holder) {
     route_holder['vote'] = setup;
 };
